352a8c99728248b28f4cc40b3786fe2ce9527600
3FE 3LE (Frank Licona) <3l33f3@gmail.com>
2020-09-21 19:17:47 -0500

    fix responsive

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -20,7 +20,7 @@ const NavBar = () => {
           <h1 className="text-3xl font-black text-red-600">SOD</h1>
           <h1 className="text-3xl font-cookie text-gray-800  -mt-8">Shop of details</h1>
         </div>
-        <div className="hidden md:flex w-full items-center justify-between">
+        <div className="hidden lg:flex w-full items-center justify-between">
           <ul className="flex items-center " >
             <li className="px-4 uppercase font-semibold">inicio</li>
             <li className="px-4 uppercase font-semibold">cajas</li>
@@ -42,7 +42,7 @@ const NavBar = () => {
             </svg>
           </div>
         </div>
-        <button onClick={toggleDrawer(true)} className="md:hidden p-2">
+        <button onClick={toggleDrawer(true)} className="lg:hidden p-2">
           <svg className="h-6 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
--- components/PromoBanner.jsx
+++ components/PromoBanner.jsx
@@ -4,9 +4,9 @@ const PromoBanner = () => {
   return (
     <>
       <div className="w-full h-full flex justify-center  ">
-        <div className="max-w-screen-lg flex flex-wrap justify-between items-stretch  py-20 px-4 w-full h-full">
-          <div className="w-1/2 bg-white bg-center bg-cover" style={{ backgroundImage: "url('https://colorlib.com/preview/theme/ashion/img/discount.jpg')" }}></div>
-          <div className="w-1/2  py-24 bg-gray-300 flex flex-wrap items-center justify-center">
+        <div className="max-w-screen-lg flex flex-wrap justify-center items-stretch  py-20 px-4 w-full h-full">
+          <div className="w-10/12 md:w-1/2 py-48 md:py-0 bg-white bg-center bg-cover" style={{ backgroundImage: "url('https://colorlib.com/preview/theme/ashion/img/discount.jpg')" }}></div>
+          <div className="w-10/12 md:w-1/2  py-24 bg-gray-300 flex flex-wrap items-center justify-center">
             <div className="rounded-full h-48 w-48 relative bg-white flex flex-wrap justify-center items-center">
               <h2 className="uppercase text-sm text-center p-0 m-0 absolute top-0 mt-12">discount</h2>
               <h1 className="text-red-600 text-center font-cookie text-6xl p-0 m-0 absolute w-80">Navidad 2020</h1>
@@ -14,19 +14,19 @@ const PromoBanner = () => {
             </div>
             <div className="w-full flex justify-center">
               <div>
-                <p className="px-6 text-2xl font-semibold" >29<span className="text-xs">days</span></p>
+                <p className="px-2 xs:px-6 text-2xl font-semibold" >29<span className="text-xs">days</span></p>
 
               </div>
               <div>
-                <p className="px-6 text-2xl font-semibold" >13<span className="text-xs">h</span></p>
+                <p className="px-2 xs:px-6 text-2xl font-semibold" >13<span className="text-xs">h</span></p>
 
               </div>
               <div>
-                <p className="px-6 text-2xl font-semibold" >09<span className="text-xs">m</span></p>
+                <p className="px-2 xs:px-6 text-2xl font-semibold" >09<span className="text-xs">m</span></p>
 
               </div>
               <div>
-                <p className="px-6 text-2xl font-semibold" >45<span className="text-xs">s</span></p>
+                <p className="px-2 xs:px-6 text-2xl font-semibold" >45<span className="text-xs">s</span></p>
 
               </div>
 
