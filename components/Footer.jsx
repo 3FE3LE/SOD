2cf9c45c40f981e30f741ffc35e6ab56524e1a3e
3FE 3LE (Frank Licona) <3l33f3@gmail.com>
2020-09-27 20:16:12 -0500

    little updates add my signature

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
+
+  const year = new Date().getFullYear();
+
+  console.log(year)
+
   return (
     <>
       <div className="w-full flex flex-wrap justify-center">
@@ -19,21 +24,19 @@ const Footer = () => {
           </div>
         </div>
         <div className="max-w-screen-xl h-full w-full flex flex-wrap justify-center px-4 pt-8">
-          <div className="sm:w-1/2 md:w-4/12 my-4 ">
+          <div className="w-full sm:w-1/2 md:w-4/12 my-4 ">
             <h1 className="text-3xl font-black text-red-600">SOD</h1>
             <h1 className="text-3xl font-cookie text-gray-800  -mt-8">Shop of details</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sapiente, architecto neque recusand</p>
+            <p className="text-sm sm:pr-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sapiente, architecto neque recusand</p>
             <div className="flex my-2">
-              <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700">
-                <i className="fab fa-facebook-f text-gray-800"></i>
-              </div>
+              <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
               <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
               <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
               <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
               <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
             </div>
           </div>
-          <div className="w-full sm:w-1/2 md:w-1/4 flex justify-around">
+          <div className="w-full sm:w-1/2 md:w-1/3 flex justify-around">
 
             <div className="sm:w-1/2 my-2">
               <h1 className="uppercase font-semibold my-4">Enlaces rápidos</h1>
@@ -57,20 +60,21 @@ const Footer = () => {
           </div>
           <div className="w-full sm:w-full md:w-4/12 my-2">
             <h1 className="uppercase font-semibold my-4">Newsletter</h1>
-            <div className="relative flex flex-wrap justify-center my-2">
-              <input className="p-4 my-2 rounded-full border w-full" type="text" />
-              <input className="p-4 my-2 sm:right-0 sm:absolute w-64 sm:w-auto rounded-full bg-red-600 text-white " type="button" value="Enviar" />
+            <div className="relative flex flex-wrap justify-center items-center my-2">
+              <input className="p-3 pl-5 my-2 rounded-full border w-full" placeholder="Email" type="email" />
+              <input className="py-3 px-4 my-2 font-bold text-sm sm:right-0 sm:mr-1 sm:absolute w-64 sm:w-auto rounded-full bg-red-600 text-white " type="button" value="SUSCRIBETE" />
             </div>
             <div className="flex my-2">
-              <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700">
-                <i className="fab fa-facebook-f text-gray-800"></i>
-              </div>
+              <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
               <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
               <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
               <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
               <div className="h-10 w-10 m-1 rounded-full bg-gray-200 text-gray-700"></div>
             </div>
           </div>
+        </div>
+        <div className="max-w-screen-xl w-full border-t text-sm">
+          <p className="w-full text-center py-2">Copyright © {year} All rights reserved | This page is code by <span className="font-black">3FE 3lE</span></p>
         </div>
       </div>
     </>
