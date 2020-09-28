579fd6061ea72833830cd2a589ace2dd7c2fc2bc
3FE 3LE (Frank Licona) <3l33f3@gmail.com>
2020-09-27 20:23:11 -0500

    little updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -74,7 +74,9 @@ const Footer = () => {
           </div>
         </div>
         <div className="max-w-screen-xl w-full border-t text-sm">
-          <p className="w-full text-center py-2">Copyright © {year} All rights reserved | This page is code by <span className="font-black">3FE 3lE</span></p>
+          <p className="w-full text-center py-2 flex justify-center">Copyright © {year} All rights reserved | This page is code with <svg className="text-red-500 h-4 w-4 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+          </svg> by <span className="font-black mx-1">3FE 3lE</span></p>
         </div>
       </div>
     </>
